778c35f7537d9f3a58886a7472db520806392b13
pet221 <erin@peterson-consulting.com>
2024-11-07 14:12:10 +1000

    updated main function

--- inst/createBigDistMatEx.R
+++ inst/createBigDistMatEx.R
@@ -15,7 +15,7 @@ mf04p <- ssn_import("c:/temp/test.ssn", predpts = "pred1km", overwrite = TRUE)
 mf04p <- ssn_update_path(mf04p, paste0(tempdir(),'/MiddleFork04.ssn'))
 ##createBigDistMat(mf04p, predpts = "pred1km", o.write = TRUE, no.cores = 1)
 
-ssn_create_distmat(mf04p, predpts = "pred1km")
+ssn_create_distmat(mf04p, predpts = "pred1km", among_predpts = TRUE, overwrite = TRUE)
 
 ssn_create_bigdist(mf04p, predpts = "pred1km", overwrite = TRUE,
                    among_predpts = TRUE, no_cores = 2)
@@ -26,38 +26,8 @@ ssn_create_bigdist(mf04p, predpts = "pred1km", overwrite = TRUE,
 
 ###############################
 
-## RECREATE DISTANCE MATRICES BOTH WAYS AND COMPARE - DOESN'T SEEM TO BE
-## CREATING OBSXOBS OR OBS PREDS A AND B
-ssn<- ssn_import("c:/temp/MiddleFork04.ssn", predpts = c("pred1km",
-                                                         "CapeHorn",
-                                                         "Knapp"),
-                 overwrite = TRUE)
-
-ssn_create_bigdist(ssn, predpts = "pred1km", overwrite = TRUE,
-                   among_predpts = TRUE, no_cores = 5)
-ssn_create_bigdist(ssn, predpts = "CapeHorn", only_predpts = TRUE,
-                   overwrite = TRUE,
-                   among_predpts = TRUE,
-                   no_cores = 5)
-ssn_create_bigdist(ssn, predpts = "Knapp", only_predpts = TRUE,
-                   overwrite = TRUE,
-                   among_predpts = TRUE,
-                   no_cores = 5)
-
-ssn_create_distmat(ssn, predpts = c("pred1km", "CapeHorn", "Knapp"),
-                   among_predpts = TRUE, overwrite = TRUE)
-
-## Figure out how to get rid of warning messages closing unused connections
-old.pred <- ssn_get_stream_distmat(ssn, "pred1km")
-
-net1a.old <- old.pred[[1]]
-net1b.old <- old.pred[[2]]
-dim(net1a.old)
-dim(net1b.old)
-## ALL PREDICTION AND OBSERVATION DISTANCES == 0
-
 library(filematrix)
-net1a.fm <- fm.open("c:/temp/test.ssn/distance/pred1km/dist.net1.a.bmat")
+net1a.fm <- fm.open("c:/temp/test.ssn/distance.fm/pred1km/dist.net1.a.bmat")
 net1a <-t(net1a.fm[,])
 dim(net1a)
 net1a
@@ -66,24 +36,48 @@ close(net1a.fm)
 sum(net1a != net1a.old)
 max(net1a - net1a.old)
 
-net1b.fm <- fm.open("c:/temp/test.ssn/distance/pred1km/dist.net1.b.bmat")
+net1b.fm <- fm.open("c:/temp/test.ssn/distance.fm/pred1km/dist.net1.bmat")
 net1b <-(net1b.fm[,])
 dim(net1b)
 net1b
 close(net1b.fm)
 
 ###########
-old.obs <- ssn_get_stream_distmat(ssn, "obs")
-
+old.obs <- ssn_get_stream_distmat(mf04p, "obs")
+names(old.obs)
 net1.old <- old.obs[[1]]
 dim(net1.old)
 
-net1.fm <- fm.open("c:/temp/test.ssn/distance/pred1km/dist.net1.bmat")
+library(filematrix)
+net1.fm <- fm.open("c:/temp/test.ssn/distance.fm/obs/dist.net1.bmat")
+net1 <-(net1.fm[,])
+dim(net1)
+net1
+max(net1 - net1.old)
+close(net1.fm)
+
+##
+old.preds <- ssn_get_stream_distmat(mf04p, "pred1km")
+
+net1.old <- old.preds[[3]]
+dim(net1.old)
+
+library(filematrix)
+net1.fm <- fm.open("c:/temp/test.ssn/distance.fm/pred1km/dist.net1.bmat")
 net1 <-(net1.fm[,])
 dim(net1)
 net1
 close(net1.fm)
 
 max(net1 - net1.old)
+
+## parLapply
+clusterEvalQ(cl, {
+  library(SSN2)
+  library(filematrix)
+})
+parLapply(cl, obs.pids, function(x) amongSitesBigDistMat(ssn = ssn, pids = x,
+                                                         bin.table = bin.table,
+                                                         name = "obs", workspace.name = workspace.name1))
 
 
