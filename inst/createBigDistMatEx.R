4d9beb7287526ddfbc6118506c566133a6a3da22
pet221 <erin@peterson-consulting.com>
2024-11-22 10:07:50 +1000

    modified example

--- inst/createBigDistMatEx.R
+++ inst/createBigDistMatEx.R
@@ -14,45 +14,48 @@ mf04p <- ssn_import("c:/temp/MiddleFork04.ssn",
                     predpts = c("pred1km", "CapeHorn"),
                     overwrite = TRUE)
 
-
+## Does not currently work with multiple sets of prediction points
 ssn_create_distmat(mf04p, predpts = c("pred1km", "CapeHorn"),
-                   among_predpts = TRUE, overwrite = TRUE)
+                    among_predpts = TRUE, overwrite = TRUE)
 
 ssn_create_bigdist(mf04p, predpts = "pred1km", overwrite = TRUE,
                    among_predpts = TRUE, no_cores = 2)
 
 ssn_create_bigdist(mf04p,
-                   predpts = c("CapeHorn"),
+                   predpts = "CapeHorn",
                    overwrite = TRUE,
                    among_predpts = TRUE,
                    no_cores = 2)
 
-# NOT RUN include prediction to prediction site distances
-      # createBigDistMat(mf04p, predpts = "pred1km", o.write = TRUE,
-      #     amongpreds = TRUE, no.cores = 4)
-
 ###############################
-## Check predictions
+## Check prediction site distances
 old.preds <- ssn_get_stream_distmat(mf04p, "CapeHorn")
 names(old.preds)
-# net1a.old <- old.preds[[1]]
-# net1b.old <- old.preds[[2]]
-# net1.old <- old.preds[[3]]
 net2a.old <- old.preds[[1]]
 net2b.old <- old.preds[[2]]
 net2.old <- old.preds[[3]]
-dim(net1.preds)
+dim(net2a.old)
 
 ## Network 1
 library(filematrix)
-net1a.fm <- fm.open("c:/temp/MiddleFork04.ssn/distance.fm/CapeHorn/dist.net1.a.bmat")
-net1a <-t(net1a.fm[,])
-dim(net1a)
-net1a
+net2a.fm <- fm.open("c:/temp/MiddleFork04.ssn/distance.fm/CapeHorn/dist.net2.a.bmat")
 
-sum(net1a != net1a.old)
-max(net1a - net1a.old)
-close(net1a.fm)
+## select by pid
+ind<- rownames(net2a.fm) %in% c("1494", "1495")
+test<- net2a.fm[ind,]
+
+net2a <-t(net2a.fm[,]) ## MUST transpose for A's only
+dim(net2a)
+net2a
+
+net2a[1:5,1:5]
+
+sum(net2a != net2a.old)
+max(net2a - net2a.old)
+close(net2a.fm)
+
+
+
 
 net1b.fm <- fm.open("c:/temp/MiddleFork04.ssn/distance.fm/CapeHorn/dist.net1.b.bmat")
 net1b <-(net1b.fm[,])
